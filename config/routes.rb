4805335f49fcaf32a364d7e3df8821ba60766484
Nanda Kishore KH <nandakishore323@gmail.com>
2020-03-06 23:14:00 +0530

    add footer and basic frontend for footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "about", to: "pages#about"
+  get "contact_us", to: "pages#contact_us"
   resources :products
   devise_for :users
   root to: 'products#index'
