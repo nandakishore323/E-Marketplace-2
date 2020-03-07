cd927050521d3d08f0290aecf20a27c11c3067dc
adityagamer786 <adityagamers786@gmail.com>
2020-03-07 22:26:08 +0530

    Sorted products by created at

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.all.order(created_at: :desc)
   end
 
   # GET /products/1
