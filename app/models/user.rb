b7aea3c295bf19043b495e42512a0d863f15d9ab
Nanda Kishore KH <nandakishore323@gmail.com>
2020-03-11 22:37:24 +0530

    minor changes to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+         validates :name, :preferred_name, presence: true
 
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider , uid: provider_data.uid).first_or_create do |user|
