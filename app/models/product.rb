d5964873aff02a026f5d73284099e89407362747
adityagamer786 <adityagamers786@gmail.com>
2020-03-11 22:11:15 +0530

    Added bootstrap classes to image

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_one_attached :image
+
+  def image_tag
+    cl_image_tag(self.image.key, width: 730, crop: "fit")
+  end
+
+  def thumb_tag
+    cl_image_tag(self.image.key, width: 300, crop: "fit")
+  end
 end
