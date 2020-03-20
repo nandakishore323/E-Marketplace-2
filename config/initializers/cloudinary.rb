961f536955b3d4f7d1f3152eebde024e1009c205
adityagamer786 <adityagamers786@gmail.com>
2020-03-20 15:48:11 +0530

    temproarily added random cloudinary api key to avoid error

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+   config.cloud_name = "sample"
+   config.api_key = "98785549878945"
+   config.api_secret = "3s258d44f1s2a22g22d23a3s3d22a3d2"
+   config.cdn_subdomain = true
+end
